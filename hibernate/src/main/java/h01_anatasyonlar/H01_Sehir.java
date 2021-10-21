c5a74505a23f222a26e223f2b33522167308aded
Capsalt <hbasri2304@gmail.com>
2021-10-21 13:27:06 +0300

    start

---
+++ hibernate/src/main/java/h01_anatasyonlar/H01_Sehir.java
@@ -0,0 +1,71 @@
+package h01_anatasyonlar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//================================================================
+//HIBERNATE ICIN TABLO TANIMLAMASININ YAPILMASI 
+//tablo oluşturabilmek için içerik hazırlama )alttakiler)
+//POJO: Plain Old Java Object									
+//1) Private degiskenler tanimlanir.
+//2) constructor olusturulur.
+//3) Getter ve Setter metotlar tanimlanir.
+//4) toString() metodu ile nesne yazdirilabilir hale getirilir.
+//================================================================
+/*
+	Bir Class Hibernate ile tablo olusturmada kullanilacaksa degiskenlerin
+	“final” veya “static” tanimlanMAMAsi gerekir. 
+*/
+//- @Entity anotasyonu bu class’ın bir tabloya donusturulmesi
+//gerektigini Hibernate’e gosterir.(sql ortamına mapping yapıo uyarlıo,translate) Her bir degisken bir sutun olacaktir.
+//- @Table annotasyonu tablo adinin degistirlmesini saglar.javada ve sql deki isimler aynı olmak zorunda değiliz
+//- @Id anotasyonu tablo icerisinde primary key olusturur.
+//- @Transient anotasyonu bir degiskenin (sutun) tabloda yer almayacagini gosterir.
+
+@Entity
+@Table(name="City")
+public class H01_Sehir {
+	
+	@Id
+	@Column(name="sehirPlaka")
+	private int cityPlaka;
+	
+	private String sehirAd;
+	
+	
+
+	public H01_Sehir(int cityPlaka, String sehirAd) {
+		super();
+		this.cityPlaka = cityPlaka;
+		this.sehirAd = sehirAd;
+	}
+
+	
+	
+	public int getCityPlaka() {
+		return cityPlaka;
+	}
+
+	public void setCityPlaka(int cityPlaka) {
+		this.cityPlaka = cityPlaka;
+	}
+
+	public String getSehirAd() {
+		return sehirAd;
+	}
+
+	public void setSehirAd(String sehirAd) {
+		this.sehirAd = sehirAd;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "H01_Sehir [cityPlaka=" + cityPlaka + ", sehirAd=" + sehirAd + "]";
+	}
+	
+
+}
