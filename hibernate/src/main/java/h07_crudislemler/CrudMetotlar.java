deaddbe825e7e83841b69916239da6c5d509c197
Capsalt <hbasri2304@gmail.com>
2021-10-27 20:09:34 +0300

    27.10

--- hibernate/src/main/java/h07_crudislemler/CrudMetotlar.java
+++ hibernate/src/main/java/h07_crudislemler/CrudMetotlar.java
@@ -1,5 +1,146 @@
 package h07_crudislemler;
 
-public class CrudMetotlar {
+import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class CrudMetotlar {
+	
+	private static SessionFactory sf;
+	
+	public void sessionFactoryOlustur () {
+		
+		
+		try {
+			Configuration con = new Configuration().configure("hibernate.cfg.xml").
+					addAnnotatedClass(Personel.class);
+				
+				sf = con.buildSessionFactory();
+		} catch (Throwable e) {
+			System.err.println("Session oluşurkekn hata oluştu" + e);
+			
+			throw new ExceptionInInitializerError();
+		}
+		
+			
+	}
+
+	public void personelEkle(String ad, String soyAd, int maas) {
+		
+		Session s = sf.openSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = s.beginTransaction();
+			Personel personel = new Personel(ad, soyAd, maas);
+			s.save(personel);
+			
+			tx.commit();
+			
+			System.out.println("Personel Kaydoldu");
+			
+		} catch (HibernateException e) {
+			if (tx!=null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+			
+		} finally {
+			s.close();
+			
+		}
+	}
+	
+	
+	public void personelListele() {
+		
+		Session s = sf.openSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = s.beginTransaction();
+			List<Personel> personeller = s.createQuery("FROM Personel").getResultList();
+			
+			personeller.stream().forEach((x)->System.out.println(x));
+			
+			tx.commit();
+			
+		} catch (HibernateException e) {
+			if (tx!=null) {
+				tx.rollback();
+		}
+			e.printStackTrace();
+			
+		}finally {
+			s.close();
+		}	
+	}
+	
+	public void personelSil(Long id) {
+		
+		Session s = sf.openSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = s.beginTransaction();
+			
+			Personel personel = s.get(Personel.class, id);
+			if (personel == null) {
+				System.out.println("Personel bulunamadı");
+			}else {
+				s.delete(personel);
+				tx.commit();
+				System.out.println(personel + "Silindi");
+			}				
+		} catch (HibernateException e) {
+			if (tx!=null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+			
+		} finally {
+			s.close();
+			
+		}
+	}
+	
+	public void maasGuncelle(Long id, int maas) {
+		Session s = sf.openSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = s.beginTransaction();
+			
+			Personel personel = s.get(Personel.class, id);
+			
+			if (personel == null) {
+				System.out.println("Personel bulunamadı");
+			}else {
+				personel.setMaas(maas);
+				tx.commit();
+				System.out.println("Maaş Güncellendi");
+			}
+							
+		} catch (HibernateException e) {
+			if (tx!=null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+			
+		} finally {
+			s.close();
+			
+		}
+		
+		
+	}
 }
+
--- hibernate/src/main/java/h07_crudislemler/Runner.java
+++ hibernate/src/main/java/h07_crudislemler/Runner.java
@@ -3,7 +3,23 @@ package h07_crudislemler;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		CrudMetotlar metod = new CrudMetotlar();
+		metod.sessionFactoryOlustur();
+		
+		metod.personelEkle("Ali", "Veli", 5000);
+		metod.personelEkle("Hasan", "Hüseyin", 6000);
+		metod.personelEkle("Ayşe", "Fatma", 7000);
+		
+	
+		metod.personelSil(7L);
+		
+		metod.personelSil(10L);
+			
+		metod.maasGuncelle(5L, 12000);
+
+		metod.personelListele();
 
 	}
 
